6f68723cd1c9199c6fab74b98fb95b920c96469f
Andrea Figueroa <andrea.figue@gmail.com>
2025-02-06 23:49:49 -0800

    new files

---
+++ t2050/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/t2050/' : '',
+  basePath: isProd ? '/t2050' : '',
+  output: 'export'
+};
+
+
+export default nextConfig;
